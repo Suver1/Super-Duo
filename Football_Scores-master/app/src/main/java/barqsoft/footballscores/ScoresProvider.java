90b97c807dbf6c13289cbe00d153e328b324a9bd
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-09-20 18:46:44 +0200

    commented.

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/ScoresProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/ScoresProvider.java
@@ -7,12 +7,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.util.Log;
 
 /**
  * Created by yehya khaled on 2/25/2015.
  */
 public class ScoresProvider extends ContentProvider
 {
+    private static final String LOG_TAG = ScoresProvider.class.getSimpleName();
     private static ScoresDBHelper mOpenHelper;
     private static final int MATCHES = 100;
     private static final int MATCHES_WITH_LEAGUE = 101;
@@ -65,6 +67,10 @@ public class ScoresProvider extends ContentProvider
     public boolean onCreate()
     {
         mOpenHelper = new ScoresDBHelper(getContext());
+
+        // Delete table
+        //SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        //db.delete(DatabaseContract.SCORES_TABLE,null,null);
         return false;
     }
 
@@ -96,19 +102,20 @@ public class ScoresProvider extends ContentProvider
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder)
     {
         Cursor retCursor;
-        //Log.v(FetchScoreTask.LOG_TAG,uri.getPathSegments().toString());
+        //Log.v(LOG_TAG,uri.getPathSegments().toString());
         int match = match_uri(uri);
-        //Log.v(FetchScoreTask.LOG_TAG,SCORES_BY_LEAGUE);
-        //Log.v(FetchScoreTask.LOG_TAG,selectionArgs[0]);
-        //Log.v(FetchScoreTask.LOG_TAG,String.valueOf(match));
+        //Log.v(LOG_TAG,SCORES_BY_LEAGUE);
+        //Log.v(LOG_TAG,selectionArgs[0]);
+        //Log.v(LOG_TAG,String.valueOf(match));
         switch (match)
         {
             case MATCHES: retCursor = mOpenHelper.getReadableDatabase().query(
                     DatabaseContract.SCORES_TABLE,
                     projection,null,null,null,null,sortOrder); break;
             case MATCHES_WITH_DATE:
-                    //Log.v(FetchScoreTask.LOG_TAG,selectionArgs[1]);
-                    //Log.v(FetchScoreTask.LOG_TAG,selectionArgs[2]);
+                    /*for ( String arg : selectionArgs) {
+                        Log.e(LOG_TAG, arg + "");
+                    }*/
                     retCursor = mOpenHelper.getReadableDatabase().query(
                     DatabaseContract.SCORES_TABLE,
                     projection,SCORES_BY_DATE,selectionArgs,null,null,sortOrder); break;
@@ -120,13 +127,13 @@ public class ScoresProvider extends ContentProvider
                     projection,SCORES_BY_LEAGUE,selectionArgs,null,null,sortOrder); break;
             default: throw new UnsupportedOperationException("Unknown Uri" + uri);
         }
+        // Tell the cursor what uri to watch, so it knows when it's source data changes
         retCursor.setNotificationUri(getContext().getContentResolver(),uri);
         return retCursor;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-
         return null;
     }
 
@@ -156,6 +163,9 @@ public class ScoresProvider extends ContentProvider
                 } finally {
                     db.endTransaction();
                 }
+                // When updated content is inserted to the db, the ContentProvider notifies the
+                // ContentResolver about the changes with notifyChange(). ContentResolver then
+                // notifies all registered observers which forces the observers to load new data.
                 getContext().getContentResolver().notifyChange(uri,null);
                 return returncount;
             default:
