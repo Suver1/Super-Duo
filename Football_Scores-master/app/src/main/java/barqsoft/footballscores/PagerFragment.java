391d94473501be8cb03bd6ded074a0aa5c314839
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-09-21 22:10:15 +0200

    Added custom color to Pager's tab strip

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.text.format.Time;
 import android.util.Log;
@@ -15,6 +16,7 @@ import android.view.ViewGroup;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by yehya khaled on 2/27/2015.
@@ -22,29 +24,30 @@ import java.util.Date;
 public class PagerFragment extends Fragment
 {
     private static final String LOG_TAG = PagerFragment.class.getSimpleName();
-    public static final int NUM_PAGES = 5;
+    public static final int NUM_PAGES = 10;
     public ViewPager mPagerHandler;
     private myPageAdapter mPagerAdapter;
-    private MainScreenFragment[] viewFragments = new MainScreenFragment[5];
+    private MainScreenFragment[] viewFragments = new MainScreenFragment[NUM_PAGES];
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
+        final View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
         mPagerHandler = (ViewPager) rootView.findViewById(R.id.pager);
-        Log.e(LOG_TAG, "running");
         mPagerAdapter = new myPageAdapter(getChildFragmentManager());
         for (int i = 0;i < NUM_PAGES;i++)
         {
             Date fragmentDate = new Date(System.currentTimeMillis()+((i-2)*86400000));
-            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
             viewFragments[i] = new MainScreenFragment();
-            viewFragments[i].setFragmentDate(mformat.format(fragmentDate));
+            viewFragments[i].setFragmentDate(format.format(fragmentDate));
         }
         mPagerHandler.setAdapter(mPagerAdapter);
         mPagerHandler.setCurrentItem(MainActivity.current_fragment);
-        mPagerHandler.setFocusable(true);
-        mPagerHandler.setNextFocusRightId(MainActivity.current_fragment + 1);
-        mPagerHandler.setNextFocusLeftId(MainActivity.current_fragment - 1);
+
+        // Set color to Pager's tab strip
+        PagerTabStrip pagerTabStrip = (PagerTabStrip) rootView.findViewById(R.id.pager_header);
+        pagerTabStrip.setTabIndicatorColorResource(R.color.teal05);
+
         return rootView;
     }
     private class myPageAdapter extends FragmentStatePagerAdapter
@@ -77,8 +80,11 @@ public class PagerFragment extends Fragment
 
             Time t = new Time();
             t.setToNow();
+            //Log.e(LOG_TAG, "time now: " + t.toString());
             int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
+            //Log.e(LOG_TAG, "julianDay: " + julianDay);
             int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
+            //Log.e(LOG_TAG, "currentJulianDay: " + currentJulianDay);
             if (julianDay == currentJulianDay) {
                 return context.getString(R.string.today);
             } else if ( julianDay == currentJulianDay +1 ) {
@@ -94,6 +100,7 @@ public class PagerFragment extends Fragment
                 time.setToNow();
                 // Otherwise, the format is just the day of the week (e.g "Wednesday".
                 SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                //Log.e(LOG_TAG, "dayFormat: " + dayFormat);
                 return dayFormat.format(dateInMillis);
             }
         }
