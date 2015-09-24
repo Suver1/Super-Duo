7fb96da6904114829c29f860eef8e516577bcad3
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-09-24 22:21:32 +0200

    Added files for collection widget

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetProvider.java
@@ -0,0 +1,9 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Andreas on 24.09.2015.
+ */
+public class WidgetProvider extends AppWidgetProvider{
+}
