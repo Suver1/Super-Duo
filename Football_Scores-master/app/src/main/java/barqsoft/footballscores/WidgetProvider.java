2a312191d62b4a6adeb8f6a88555cffaca0f91bc
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-09-26 21:54:09 +0200

    Added basic Widget Provider

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetProvider.java
@@ -1,9 +1,22 @@
 package barqsoft.footballscores;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 /**
  * Created by Andreas on 24.09.2015.
  */
-public class WidgetProvider extends AppWidgetProvider{
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
 }
