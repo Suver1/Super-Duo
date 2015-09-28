519f6653870169b13cd8e596b2a40eef205c4900
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-09-28 22:03:50 +0200

    parse data and update widget

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetProvider.java
@@ -4,11 +4,21 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.service.WidgetService;
 
 /**
  * Created by Andreas on 24.09.2015.
+ * Sources: http://developer.android.com/guide/topics/appwidgets/index.html
+ * http://dharmangsoni.blogspot.no/2014/03/collection-widget-with-event-handling.html
+ *
+ * AppWidgetProvider parses relevant fields out of the Intent that is received in onReceive().
  */
 public class WidgetProvider extends AppWidgetProvider {
+    private static final String LOG_TAG = WidgetProvider.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -17,6 +27,18 @@ public class WidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_provider_layout);
+            Intent intent = new Intent(context, WidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            remoteViews.setRemoteAdapter(widgetId, R.id.widgetCollectionList, intent);
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 }
